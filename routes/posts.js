175bff6bb36f032ae691e669544d1f7489fd3341
Zharne <zharnedesember@gmail.com>
2022-03-16 15:53:48 +0200

    async

--- routes/posts.js
+++ routes/posts.js
@@ -61,5 +61,19 @@ router.delete('/:postId', async (req,res) => {
         res.json({ message: err });
     }
 });
-
+async function getPost(req, res, next) {
+    let post;
+    try {
+      post = await Post.findById(req.params.id);
+      if (post == null) {
+        return res.status(404).json({ message: "Cannot find product" });
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
+    }
+  
+    res.post = post;
+    next();
+  }
+  
 module.exports = router;
